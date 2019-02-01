dd7cf6702577e73dfcb933077938c8c6a5ebc706
Sebastian Andil <sebastian.andil@gmail.com>
2019-02-01 21:34:43 +0100

    Remove normalize.css in favor of sanitize.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'normalize.css'
 import 'sanitize.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
