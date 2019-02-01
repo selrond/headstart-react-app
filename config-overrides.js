e705e0fa45e06fcab4955d4cbbab7c80fad5179b
Sebastian Andil <sebastian.andil@gmail.com>
2019-02-01 21:53:00 +0100

    Remove react-app-rewired & babel-plugin-styled-components

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const { injectBabelPlugin } = require('react-app-rewired')
-
-module.exports = (config) => {
-  // eslint-disable-next-line no-param-reassign
-  config = injectBabelPlugin(['styled-components'], config)
-  return config
-}
