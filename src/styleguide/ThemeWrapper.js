8fe523fe2df968ecac407b18e679643d1dbe76af
Sebastian Andil <sebastian.andil@gmail.com>
2019-02-01 21:46:44 +0100

    Remove styleguide

--- src/styleguide/ThemeWrapper.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { ThemeProvider } from 'styled-components'
-import theme from 'theme'
-
-const ThemeWrapper = props => <ThemeProvider theme={theme} {...props} />
-
-export default ThemeWrapper
